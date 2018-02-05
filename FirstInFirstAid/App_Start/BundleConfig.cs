6948517ab8e8ad789809f280c5bd9ca440585118
Anu <anurada914@gmail.com>
2018-02-05 12:26:11 +1030

    Add the jquery-ui script and css bundle

--- FirstInFirstAid/App_Start/BundleConfig.cs
+++ FirstInFirstAid/App_Start/BundleConfig.cs
@@ -13,6 +13,13 @@ namespace FirstInFirstAid
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+            // Add the jquery-ui script bundle
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            // Add the jquery-ui css bundle
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/all.css"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
