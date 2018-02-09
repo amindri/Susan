134a581d53f364e445db7cd0e83e918a01c08445
Anu <anurada914@gmail.com>
2018-02-09 15:15:28 +1030

    No changes

--- FirstInFirstAid/Models/Trainor.cs
+++ FirstInFirstAid/Models/Trainor.cs
@@ -13,7 +13,7 @@ namespace FirstInFirstAid.Models
         public string Lastname { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
-        public DateTime DOB { get; set; }      
+        public DateTime DOB { get; set; } 
         public string TaxFileNo { get; set; }
 
         public Address Address { get; set; }
