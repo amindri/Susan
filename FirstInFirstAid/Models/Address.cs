9fa270a7b7f06454cdd48dd189bcff93dc143fc8
Amindri Udugla <amindriudugala@gmail.com>
2018-03-01 23:19:38 +1030

    Adding a Enum for states

--- FirstInFirstAid/Models/Address.cs
+++ FirstInFirstAid/Models/Address.cs
@@ -1,19 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace FirstInFirstAid.Models
 {
+    public enum State { SA, NT, WA, QLD, ACT, NSW, VIC, TAS }
     public class Address
-    {
+    {     
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Street Number missing")]
         public string StreetNumber { get; set; }
+
+        [Required(ErrorMessage = "Street Name missing")]
         public string StreetName { get; set; }
+
+        [Required(ErrorMessage = "Suburb missing")]
         public string Suburb { get; set; }
+
+        [Required(ErrorMessage = "Postcode missing")]
         public string Postcode { get; set; }
-        public string State { get; set; }
+
+        [Required(ErrorMessage = "State missing")]
+        public State? State { get; set; }
     }
 }
