8196de62faf5d238bc01c4feaf0ba4e28d52783e
Amindri Udugla <amindriudugala@gmail.com>
2018-03-07 23:49:43 +1030

    extending the custom combobox widget  to get different  dropdown options in different pages

--- FirstInFirstAid/Scripts/jquery-combobox.js
+++ FirstInFirstAid/Scripts/jquery-combobox.js
@@ -44,76 +44,7 @@
             });
         },
 
-        _createShowAllButton: function () {
-            var input = this.input,
-                wasOpen = false;
-
-            $("<a>")
-                .attr("tabIndex", -1)
-                .attr("title", "Show All Items")
-                .tooltip()
-                .appendTo(this.wrapper)
-                .button({
-                    icons: {
-                        primary: "ui-icon-triangle-1-s"
-                    },
-                    text: false
-                })
-                .removeClass("ui-corner-all")
-                .addClass("custom-combobox-toggle ui-corner-right")
-                .on("mousedown", function () {
-                    // new code to call ajax
-                    /*$.ajax({
-                        type: "POST",
-                        contentType: "application/json; charset=utf-8",
-                        url: "/Events/GetClients",
-                        data: "{}",
-                        dataType: "json",
-                        success: function (clientList) {
-                            clientm.empty().append($("<option selected='selected' value=''> Please Select </option>"));
-                            $.each(clientList, function (index, client) {
-                                clientm.append($("<option></option>").attr("value", client.Id).text(client.Name));
-                            });
-                        },
-                        error: function () {
-                            alert("Failed to load Clients");
-                        }
-                    });*/
-                    //end new code
-                    wasOpen = input.autocomplete("widget").is(":visible");
-                })
-                .on("click", function () {
-                    // new code to call ajax
-                    /*$.ajax({
-                        type: "POST",
-                        contentType: "application/json; charset=utf-8",
-                        url: "/Events/GetClients",
-                        data: "{}",
-                        dataType: "json",
-                        success: function (clientList) {
-                            clientm.empty().append($("<option selected='selected' value=''> Please Select </option>"));
-                            $.each(clientList, function (index, client) {
-                                clientm.append($("<option></option>").attr("value", client.Id).text(client.Name));
-                            });
-                        },
-                        error: function () {
-                            alert("Failed to load Clients");
-                        }
-                    });*/
-                    //end new code
-                    input.trigger("focus");
-
-                    // Close if already visible
-                    if (wasOpen) {
-                        return;
-                    }
-
-                    // Pass empty string as value to search for, displaying all results
-                    input.autocomplete("search", "");
-                    
-                });
-        },
-
+        
         _source: function (request, response) {
             var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
             response(this.element.children("option").map(function () {
