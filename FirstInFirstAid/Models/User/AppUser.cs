752b2a8572045fcbc9eab8e84a828ec967ff5e6f
Amindri Udugla <amindriudugala@gmail.com>
2018-04-28 01:27:26 +0930

    Reverting back to before auth state

---
+++ FirstInFirstAid/Models/User/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace FirstInFirstAid.Models.User
+{
+    public class AppUser : IdentityUser
+    {
+        public int Id { get; set; }
+        public String UserName { get; set; }
+        public String Password { get; set; }
+    }
+}
---
+++ FirstInFirstAid/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FirstInFirstAid.Startup))]
+
+namespace FirstInFirstAid
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+    }
+}
