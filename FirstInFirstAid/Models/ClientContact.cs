0f2e49d42adfe230e455f7e0d11427d948cfbaa4
Anu <anurada914@gmail.com>
2018-05-31 01:00:31 +0930

    Add 10 digit phone number validation and email format validation

--- FirstInFirstAid/Models/ClientContact.cs
+++ FirstInFirstAid/Models/ClientContact.cs
@@ -12,9 +12,14 @@ namespace FirstInFirstAid.Models
         public string ContactName { get; set; }
 
         public string ContactPosition { get; set; }
+        [RegularExpression("(\\d{10})", ErrorMessage = "Please enter a valid 10 digit phone number")]
         public string ContactPhone { get; set; }
 
         [EmailAddress(ErrorMessage = "Invalid email address. Please use \'yourname@example.com\' format")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                            @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                            @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+                            ErrorMessage = "Please enter a valid email address")]
         public string ContactEmail { get; set; }
 
         public Client Client { get; set; }
